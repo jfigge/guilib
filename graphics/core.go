56cb4d1dcd0ddbf033366769ce1e586fc14be2e2
jfigge <jason@sahara.lan>
2023-09-12 22:39:30 -0400

    Added configuration options

--- graphics/core.go
+++ graphics/core.go
@@ -33,45 +33,65 @@ type Canvas struct {
 	lock           sync.Mutex
 }
 
-func Open(title string, width, height int32, handler GUIHandler) {
+type config struct {
+	windowX       int32
+	windowY       int32
+	windowFlags   uint32
+	framerate     int
+	rendererFlags uint32
+}
+
+type ConfigOption func(c *config)
+
+func Open(title string, width, height int32, handler GUIHandler, options ...ConfigOption) {
 	sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "0")
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(fmt.Errorf("failed to started sdl: %w", err))
 	}
 
+	cfg := &config{
+		windowX:       sdl.WINDOWPOS_UNDEFINED,
+		windowY:       sdl.WINDOWPOS_UNDEFINED,
+		windowFlags:   sdl.WINDOW_OPENGL,
+		framerate:     60,
+		rendererFlags: sdl.RENDERER_ACCELERATED, //|sdl.RENDERER_PRESENTVSYNC
+	}
+	for _, option := range options {
+		option(cfg)
+	}
+
 	window, err := sdl.CreateWindow(
 		title,
-		sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED,
+		cfg.windowX,
+		cfg.windowY,
 		width, height,
-		sdl.WINDOW_OPENGL,
+		cfg.windowFlags,
 	)
 	if err != nil {
 		panic(fmt.Errorf("failed to create window: %w", err))
 	}
 	c := &Canvas{
-		Window: window,
-		state:  initialized,
-		done:   make(chan bool),
+		state: initialized,
+		done:  make(chan bool),
 	}
 
-	c.renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED) //|sdl.RENDERER_PRESENTVSYNC)
+	c.renderer, err = sdl.CreateRenderer(window, -1, cfg.rendererFlags)
 	if err != nil {
 		panic(fmt.Errorf("failed to create renderer: %w", err))
 	}
 	c.handler = handler
 	c.handler.Init(c)
-	c.start()
+	c.start(cfg.framerate)
 }
 
-func (c *Canvas) start() {
+func (c *Canvas) start(framerate int) {
 	if c.state != initialized {
 		return
 	}
 	c.state = running
 
 	c.handler.Init(c)
-	go c.gameLoop()
+	go c.gameLoop(framerate)
 
 	fmt.Println("Event loop starting")
 	for c.state == running {
@@ -109,8 +129,9 @@ func (c *Canvas) start() {
 
 }
 
-func (c *Canvas) gameLoop() {
+func (c *Canvas) gameLoop(framerate int) {
 	c.glwg.Add(1)
+	fr := time.Second / time.Duration(framerate)
 	go func() {
 		defer func() {
 			fmt.Println("Game loop Exited")
@@ -118,7 +139,7 @@ func (c *Canvas) gameLoop() {
 		}()
 		defer c.panicHandler("game loop")()
 		fmt.Println("Game loop starting")
-		c.frameRateTimer = time.NewTicker(time.Second / 60)
+		c.frameRateTimer = time.NewTicker(fr)
 		for c.state == running {
 			select {
 			case <-c.done:
@@ -179,5 +200,47 @@ func (c *Canvas) panicHandler(name string) func() {
 			c.Quit()
 		}
 		return
+	}
+}
+
+func Framerate(fr int) ConfigOption {
+	return func(c *config) {
+		if fr > 0 {
+			c.framerate = fr
+		}
+	}
+}
+
+func RendererFlags(flags uint32) ConfigOption {
+	return func(c *config) {
+		c.rendererFlags = flags &
+			sdl.RENDERER_SOFTWARE &
+			sdl.RENDERER_ACCELERATED &
+			sdl.RENDERER_PRESENTVSYNC &
+			sdl.RENDERER_TARGETTEXTURE
+	}
+}
+
+func WindowPosition(x, y int32) ConfigOption {
+	return func(c *config) {
+		c.windowX = x
+		c.windowY = y
+	}
+}
+
+func WindowFlags(flags uint32) ConfigOption {
+	return func(c *config) {
+		c.windowFlags = flags &
+			sdl.WINDOW_FULLSCREEN &
+			sdl.WINDOW_FULLSCREEN_DESKTOP &
+			sdl.WINDOW_OPENGL &
+			sdl.WINDOW_VULKAN &
+			sdl.WINDOW_HIDDEN &
+			sdl.WINDOW_BORDERLESS &
+			sdl.WINDOW_RESIZABLE &
+			sdl.WINDOW_MINIMIZED &
+			sdl.WINDOW_MAXIMIZED &
+			sdl.WINDOW_INPUT_GRABBED &
+			sdl.WINDOW_ALLOW_HIGHDPI
 	}
 }
